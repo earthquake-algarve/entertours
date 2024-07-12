3efab9ef4e052bb27d0827181e5a684f760e715e
Gustavo Evangelista <gustavo.eng.henrique@gmail.com>
2024-07-12 14:44:16 +0100

    Made categories cards as links

--- src/components/sections/CategoriesSection.tsx
+++ src/components/sections/CategoriesSection.tsx
@@ -2,25 +2,31 @@ import { MdOutlineSurfing, MdOutlineKayaking } from 'react-icons/md';
 import { PiPersonSimpleHikeBold } from 'react-icons/pi';
 import { FaSailboat, FaBus } from 'react-icons/fa6';
 import CategoryCard from '../CategoryCard';
+import Link from 'next/link';
 
 const categories = [
 	{
+		id: '1',
 		name: 'Boats',
 		icon: <FaSailboat size={40} className='mt-1' />,
 	},
 	{
+		id: '2',
 		name: 'Surf',
 		icon: <MdOutlineSurfing size={40} className='mt-1' />,
 	},
 	{
+		id: '3',
 		name: 'Kayak',
 		icon: <MdOutlineKayaking size={40} className='mt-1' />,
 	},
 	{
+		id: '4',
 		name: 'Hiking',
 		icon: <PiPersonSimpleHikeBold size={40} className='mt-1' />,
 	},
 	{
+		id: '5',
 		name: 'City Tour',
 		icon: <FaBus size={40} className='mt-1' />,
 	},
@@ -30,12 +36,13 @@ const categories = [
 export default function CategoriesSection() {
 	return (
 		<section className='flex justify-around items-center bg-orange-300'>
-			{categories.map((category, index) => (
-				<CategoryCard
-                    key={index}
-					title={category.name}
-					icon={category.icon}
-				/>
+			{categories.map((category) => (
+				<Link key={category.id} href='/'>
+					<CategoryCard
+						title={category.name}
+						icon={category.icon}
+					/>
+				</Link>
 			))}
 		</section>
 	);
