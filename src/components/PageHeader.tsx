98ccf5bd0c370916457b6a294ec27977ac38e5f8
Gustavo Evangelista <gustavo.eng.henrique@gmail.com>
2024-08-17 23:06:01 +0100

    fix: Added conditional rendering to avoid UI mistakes

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,25 +1,27 @@
-import { ReactNode } from "react";
-import { BrandButton } from "./BrandButton";
+import { ReactNode } from 'react';
+import { BrandButton } from './BrandButton';
 
 type PageHeaderProps = {
-  children: ReactNode;
-  buttonChildren: ReactNode;
-  buttonAsChild?: boolean;
+	children: ReactNode;
+	buttonChildren?: ReactNode;
+	buttonAsChild?: boolean;
 };
 
 export function PageHeader({
-  children,
-  buttonChildren,
-  buttonAsChild ,
+	children,
+	buttonChildren,
+	buttonAsChild,
 }: PageHeaderProps) {
-  return (
-    <div className="text-3xl mb-8 flex justify-between w-full">
-      {children}
-      <BrandButton
-        asChild={buttonAsChild}
-      >
-        {buttonChildren}
-      </BrandButton>
-    </div>
-  );
+	return (
+		<div className='text-3xl mb-8 flex justify-between w-full'>
+			{children}
+			{buttonChildren ? (
+				<BrandButton asChild={buttonAsChild}>
+					{buttonChildren}
+				</BrandButton>
+			) : (
+				''
+			)}
+		</div>
+	);
 }
