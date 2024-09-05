83b73c38c20ec9337b34d44784373ff7eb3c4f9f
Gustavo Evangelista <gustavo.eng.henrique@gmail.com>
2024-09-05 15:08:21 +0100

    feat: Created Tour Details page

---
+++ src/app/tours/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { PageHeader } from '@/components/PageHeader';
+import TourDetailsCard from '@/components/TourDetailsCard';
+import { getTourById } from '@/db/dbActions';
+import Link from 'next/link';
+import React from 'react';
+
+export default async function TourDetails({
+	params: { id },
+}: {
+	params: { id: string };
+}) {
+	const tour = await getTourById(id);
+
+	return (
+		<div className='border-2 p-16 flex flex-col justify-center items-center'>
+			<PageHeader
+				buttonAsChild={true}
+				buttonChildren={<Link href='/tours'>Back to all tours</Link>}
+				>
+				Tour
+			</PageHeader>
+
+			<TourDetailsCard
+				id={tour?.id}
+				title={tour?.name}
+				location={tour?.location}
+				duration={tour?.duration}
+				price={tour?.price}
+				description={tour?.description}
+			/>
+		</div>
+	);
+}
