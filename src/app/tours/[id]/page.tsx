7ac179ff303d2b330118db94d6d56e49c8a11590
Gustavo Evangelista <gustavo.eng.henrique@gmail.com>
2024-09-21 21:35:22 +0100

    feat: added image fetched from db to card

--- src/app/tours/[id]/page.tsx
+++ src/app/tours/[id]/page.tsx
@@ -27,6 +27,7 @@ export default async function TourDetails({
 				duration={tour?.duration}
 				price={tour?.price}
 				description={tour?.description}
+				imagePath={tour?.imagePath}
 			/>
 		</div>
 	);
--- src/components/TourDetailsCard.tsx
+++ src/components/TourDetailsCard.tsx
@@ -17,6 +17,7 @@ type TourDetailsCardProps = {
 	duration: number | undefined;
 	price: number;
 	description: string | undefined;
+	imagePath: string ;
 };
 
 export default function TourDetailsCard({
@@ -26,13 +27,14 @@ export default function TourDetailsCard({
 	duration,
 	price,
 	description,
+	imagePath
 }: TourDetailsCardProps) {
 	return (
 		<Card className='w-full border-none shadow-xl rounded-md flex justify-between items-center p-4 xl:w-5/6'>
 			<div className='w-[800px] xl:w-[1000px] h-[450px] relative'>
 				{/* this is going to be a carousel of images */}
 				<Image
-					src='/banner.png'
+					src={imagePath}
 					alt='card image'
 					fill
 					className='rounded-sm '
@@ -45,7 +47,7 @@ export default function TourDetailsCard({
 				<CardContent className='flex flex-col gap-6 font-semibold'>
 					<span>Location: {location}</span>
 					<span>Duration: {duration} minutes</span>
-					<span>Price: {formatCurrency(price)} per person</span>
+					<span>Price: {formatCurrency(price/100)} per person</span>
 					<span>Description: {description}</span>
 					<span>Data</span>
 					<span>Horario</span>
