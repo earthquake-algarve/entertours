8bd9942d57812ee627f1788fb2ba09327597fd23
Gustavo Evangelista <gustavo.eng.henrique@gmail.com>
2024-10-13 18:55:16 +0100

    refactor: Changed CategoryCard to display in Categories page

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,27 +1,38 @@
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import Image from 'next/image';
 
 type CategoryCardProps = {
-    title: string
-    icon:any
-}
+	title: string;
+	numberOfTours: number;
+	imagePath: string;
+};
 
-export default function CategoryCard({title, icon}: CategoryCardProps) {
-  return (
-      <Card className=" bg-orange-300 border-none mt-1">
-          <CardContent className="flex flex-col justify-center items-center">
-              <span className="font-bold items-center ">
-                  {title}
-              </span>
-              {icon}
-          </CardContent>
-      </Card>
-  )
+export default function CategoryCard({
+	title,
+	numberOfTours,
+	imagePath,
+}: CategoryCardProps) {
+	return (
+		<Card className='w-fit border-none shadow-lg rounded-md'>
+			<div className='w-72 h-56'>
+				<Image
+					src={imagePath}
+					alt='card image'
+					width={300}
+					height={230}
+					className='rounded-md w-full h-full'
+				/>
+			</div>
+			<CardHeader>
+				<CardTitle className='text-xl'>{title}</CardTitle>
+			</CardHeader>
+			<CardContent className='flex justify-between'>
+				<div className='flex flex-col'>
+					<span className=''>
+						{numberOfTours} {numberOfTours > 1 ? 'tours' : 'tour'}
+					</span>
+				</div>
+			</CardContent>
+		</Card>
+	);
 }
