b627aac493e4ca1b500a7fe69ea77690399c4228
Gustavo Evangelista <gustavo.eng.henrique@gmail.com>
2024-10-13 18:59:17 +0100

    feat: Created function to get how many tours by location

--- src/db/locations/location.ts
+++ src/db/locations/location.ts
@@ -1,17 +1,39 @@
-'use server'
+'use server';
 
-import db from "../db"
+import db from '../db';
 
-export async function getLocations(){
-    const locations = await db.location.findMany();
+export async function getLocations() {
+	const locations = await db.location.findMany({
+		where: {
+			isActive: true,
+		},
+		include: {
+			image: true,
+		},
+	});
 
-    return locations;
+	return locations;
 }
 
-export async function getLocationById(id: string){
-    const location = await db.location.findUnique({
-        where: { id: id }
-    })
-    
-    return location;
+export async function getLocationById(id: string) {
+	const location = await db.location.findUnique({
+		where: { id: id, isActive: true },
+	});
+
+	return location;
+}
+
+export async function getHowManyToursByLocation() {
+	const locations = await db.location.findMany({
+		select: {
+			id: true,
+			name: true,
+			image: true,
+			_count: { select: { tours: true } },
+		},
+		where: {
+			isActive: true,
+		},
+	});
+	return locations;
 }
