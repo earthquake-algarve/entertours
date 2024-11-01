ebcd5b3be638b1da13a1380027a4e49c6753ad34
Gustavo Evangelista <gustavo.eng.henrique@gmail.com>
2024-11-01 16:12:33 +0000

    refactor: Added link to PageHeader on New Tour page

--- src/app/company/tours/new/page.tsx
+++ src/app/company/tours/new/page.tsx
@@ -1,12 +1,17 @@
 import { PageHeader } from '@/components/PageHeader';
 import TourForm from '@/components/TourForm';
 import { Card } from '@/components/ui/card';
+import Link from 'next/link';
 
 export default function NewTour() {
 	return (
 		<>
 			<div className='flex flex-col justify-center items-center p-10'>
-				<PageHeader>Add a tour</PageHeader>
+				<PageHeader
+					buttonAsChild={true}
+					buttonChildren={<Link href='/'>Back to home page</Link>}>
+					Add a tour
+				</PageHeader>
 				<div className='flex justify-center items-center'>
 					<Card className='shadow-lg'>
 						<TourForm />
