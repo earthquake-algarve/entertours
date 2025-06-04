df86d9cd6e9c6010efb073ac663fc9586af8ddc5
Gustavo Evangelista <gustavo.eng.henrique@gmail.com>
2025-06-04 22:14:21 +0100

    refactor: rendering categories icons

--- src/components/sections/CategoriesSection.tsx
+++ src/components/sections/CategoriesSection.tsx
@@ -8,36 +8,36 @@ import { ArrowRight } from 'lucide-react';
 import { getCategories } from '@/db/category/category';
 import { Card, CardContent } from '@/components/ui/card';
 
-// const categories = [
-// 	{
-// 		id: '1',
-// 		name: 'Boats',
-// 		icon: <FaSailboat size={40} className='mt-1' />,
-// 	},
-// 	{
-// 		id: '2',
-// 		name: 'Surf',
-// 		icon: <MdOutlineSurfing size={40} className='mt-1' />,
-// 	},
-// 	{
-// 		id: '3',
-// 		name: 'Kayak',
-// 		icon: <MdOutlineKayaking size={40} className='mt-1' />,
-// 	},
-// 	{
-// 		id: '4',
-// 		name: 'Hiking',
-// 		icon: <PiPersonSimpleHikeBold size={40} className='mt-1' />,
-// 	},
-// 	{
-// 		id: '5',
-// 		name: 'City Tour',
-// 		icon: <FaBus size={40} className='mt-1' />,
-// 	},
-// ];
+const categories = [
+	{
+		id: '1',
+		name: 'Boats',
+		icon: <FaSailboat size={40} className='mt-1' />,
+	},
+	{
+		id: '2',
+		name: 'Surf',
+		icon: <MdOutlineSurfing size={40} className='mt-1' />,
+	},
+	{
+		id: '3',
+		name: 'Kayak',
+		icon: <MdOutlineKayaking size={40} className='mt-1' />,
+	},
+	{
+		id: '4',
+		name: 'Hiking',
+		icon: <PiPersonSimpleHikeBold size={40} className='mt-1' />,
+	},
+	{
+		id: '5',
+		name: 'City Tour',
+		icon: <FaBus size={40} className='mt-1' />,
+	},
+];
 
 export default async function CategoriesSection() {
-	const categories = await getCategories();
+	// const categories = await getCategories();
 	return (
 		<section className='p-4 flex flex-col bg-orange-300'>
 			<div className='flex justify-around items-center'>
@@ -48,7 +48,8 @@ export default async function CategoriesSection() {
 								<span className='font-bold items-center '>
 									{category.name}
 								</span>
-								<FaBus size={40} className='mt-1' />
+								{/* <FaBus size={40} className='mt-1' /> */}
+								{category.icon}
 							</CardContent>
 						</Card>
 					</Link>
