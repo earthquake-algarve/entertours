15dacd642a03634a331f690c83f60d4676327bab
Gustavo Evangelista <gustavo.eng.henrique@gmail.com>
2025-06-22 14:56:47 +0100

    feat: Created Carousel for TourDetailsCard images

--- src/components/TourDetailsCard.tsx
+++ src/components/TourDetailsCard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
 	Card,
 	CardContent,
@@ -9,6 +11,15 @@ import { formatCurrency } from '@/lib/formatters';
 import Image from 'next/image';
 import Link from 'next/link';
 import { BrandButton } from './BrandButton';
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselPrevious,
+	CarouselNext,
+} from '@/components/ui/carousel';
+import Autoplay from 'embla-carousel-autoplay';
+import { TourImage } from '@/types/tourImage';
 
 type TourDetailsCardProps = {
 	id: string | undefined;
@@ -17,7 +28,7 @@ type TourDetailsCardProps = {
 	duration: number | undefined;
 	price: number;
 	description: string | undefined;
-	imagePath: string ;
+	images: TourImage[];
 };
 
 export default function TourDetailsCard({
@@ -27,35 +38,87 @@ export default function TourDetailsCard({
 	duration,
 	price,
 	description,
-	imagePath
+	images,
 }: TourDetailsCardProps) {
 	return (
-		<Card className='w-full border-none shadow-xl rounded-md flex justify-between items-center p-4 xl:w-5/6'>
-			<div className='w-[800px] xl:w-[1000px] h-[450px] relative'>
-				{/* this is going to be a carousel of images */}
-				<Image
-					src={imagePath}
-					alt='card image'
-					fill
-					className='rounded-sm '
-				/>
+		<Card className='w-full max-w-5xl mx-auto border-none shadow-2xl rounded-2xl flex flex-col md:flex-row bg-white p-9'>
+			<div className='md:w-2/3 w-full flex items-center justify-center p-4'>
+				<Carousel
+					opts={{
+						align: 'center',
+					}}
+					plugins={[
+						Autoplay({
+							delay: 4000,
+						}),
+					]}
+					className='w-full max-w-2xl'>
+					<CarouselContent>
+						{images?.length > 0 ? (
+							images.map((image) => (
+								<CarouselItem key={image.id}>
+									<div className='flex justify-center items-center h-[350px] md:h-[450px]'>
+										<Image
+											src={image.name}
+											alt={image.name || 'Tour Image'}
+											height={450}
+											width={700}
+											className='rounded-xl object-cover w-full h-full shadow-lg'
+										/>
+									</div>
+								</CarouselItem>
+							))
+						) : (
+							<CarouselItem>
+								<div className='flex justify-center items-center h-[350px] md:h-[450px] bg-gray-100 rounded-xl'>
+									<span className='text-gray-400'>
+										No images available
+									</span>
+								</div>
+							</CarouselItem>
+						)}
+					</CarouselContent>
+					<CarouselPrevious />
+					<CarouselNext />
+				</Carousel>
 			</div>
-			<div className='xl:w-[500px] flex flex-col justify-center items-center'>
-				<CardHeader>
-					<CardTitle>{title}</CardTitle>
+			<div className='md:w-1/3 w-full flex flex-col justify-between p-6'>
+				<CardHeader className='pb-2'>
+					<CardTitle className='text-2xl font-bold'>
+						{title}
+					</CardTitle>
 				</CardHeader>
-				<CardContent className='flex flex-col gap-6 font-semibold'>
-					<span>Location: {location}</span>
-					<span>Duration: {duration} minutes</span>
-					<span>Price: {formatCurrency(price/100)} per person</span>
-					<span>Description: {description}</span>
-					<span>Data</span>
-					<span>Horario</span>
+				<CardContent className='flex flex-col gap-4 text-base font-medium text-gray-700'>
+					<span>
+						<span className='font-semibold text-gray-900'>
+							Location:
+						</span>{' '}
+						{location}
+					</span>
+					<span>
+						<span className='font-semibold text-gray-900'>
+							Duration:
+						</span>{' '}
+						{duration} minutes
+					</span>
+					<span>
+						<span className='font-semibold text-gray-900'>
+							Price:
+						</span>{' '}
+						{formatCurrency(price / 100)} per person
+					</span>
+					<span>
+						<span className='font-semibold text-gray-900'>
+							Description:
+						</span>{' '}
+						{description}
+					</span>
+					{/* Add more details here if needed */}
 				</CardContent>
-				<CardFooter className=' w-full flex justify-center items-center mt-12'>
+				<CardFooter className='w-full flex justify-center mt-8'>
 					<BrandButton>
 						<Link
-							className='w-52 font-bold'
+							className='w-52 font-bold text-center'
 							href={`/tour/${id}/purchase`}>
 							Purchase
 						</Link>
